5069849adb6ee65fcea61996c685242e311c73dd
CMiddendorf <cmid72795@gmail.com>
2021-06-19 01:10:16 -0500

    Forgot the database initialization code.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
+using BackEndDeveloperAssessment.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +15,14 @@ namespace BackEndDeveloperAssessment
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                scope.ServiceProvider.GetRequiredService<StudentContext>().Database.EnsureCreated();
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
